3a33fa7ee0823a6a1bd4d514df2d2c4aac2406cf
a01403177-byte <a01403177@tec.mx>
2026-04-14 12:27:23 -0600

    Update db.py

--- app/db.py
+++ app/db.py
@@ -1,9 +1,10 @@
+##Aqui se crea la base de datos con la que trabajara el sistema 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
-
+##sqlalchemy se utiliza para crear la conexion con la base de datos 
 from app.config import settings
 
-
+##Aqui se crea crea la base de datos, a la cual despues se le agregaran modelos para agregarle informacion
 class Base(DeclarativeBase):
     pass
 
